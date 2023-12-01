d64e006c7e329ce0cab63a09a276b407df20fa70
Daniel Hernandez <edhernandez946@gmail.com>
2023-12-01 13:26:59 -0600

    Initial commit

---
+++ src/lab8_p1_erickhernandez/Lab8_P1_ErickHernandez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab8_p1_erickhernandez;
+
+/**
+ *
+ * @author Erick
+ */
+public class Lab8_P1_ErickHernandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
